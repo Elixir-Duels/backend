9f62e16f22e45ce02e8f2d10d3dca6d7d20471b7
Kai Jellinghaus <contact@kaij.tech>
2022-09-14 16:53:05 +0200

    Handle Auth in socket

--- lib/duels_web/user_socket.ex
+++ lib/duels_web/user_socket.ex
@@ -1,10 +1,11 @@
 defmodule DuelsWeb.UserSocket do
   use Phoenix.Socket
 
-  def connect(_params, socket, _connect_info) do
-    # TODO: Read token here
-    user_id = Ecto.UUID.generate()
-    {:ok, assign(socket, :user_id, user_id)}
+  def connect(params, socket, _connect_info) do
+    token = params["token"]
+    with {:ok, user_id} = Phoenix.Token.verify(socket, "user auth", token) do
+      {:ok, assign(socket, :user_id, user_id)}
+    end
   end
 
   def id(socket), do: "users_socket:#{socket.assigns.user_id}"
